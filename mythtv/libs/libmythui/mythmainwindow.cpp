fphillips/mythtv

commit 39686c6c6800053fe25764d2f27cfdfc2dd21fab

Fix old Qt setting screens preventing them from accepting keyboard inputs
Hopefully this is the final fix…
The issue seems to only have occurred on macs. When opening two MythDialogs (that includes MythPopup) in a row, often the 2nd one would not accept any used input anymore, only causing a “beep”; making it impossible to even exit that window.

The existing logic (and there was nothing wrong with it) was to simply disable the current MythDialog and put the new one on top of it. However, when doing so, somehow key events are now ignored.
So instead, we now disable all widgets within that MythDialog, without disabling the dialog itself.

When doing so, the newly created MythDialog is always working as expected.
This code should not be necessary, it is a workaround to what appears to be a Qt bug.

Fixes #11990.

(cherry picked from commit 9edb6d6c4aa0ed930be222ddd32cfc008d709d9f)

mythtv/libs/libmythui/mythmainwindow.cpp
@@ -264,6 +264,7 @@ class MythMainWindowPrivate
 
     /* compatibility only, FIXME remove */
     std::vector<QWidget *> widgetList;
+    QMap<QWidget *, bool> enabledWidgets;
 
     QWidget *paintwin;
 
@@ -1322,7 +1323,22 @@ void MythMainWindow::attach(QWidget *child)
             .arg(::GetCurrentThreadId()));
 #endif
     if (currentWidget())
-        currentWidget()->setEnabled(false);
+    {
+        // don't disable the current widget, instead we disable all its children
+        // on mac, disabling the current active widget entirely prevent keyboard to
+        // work on the newly opened widget.
+        QList<QWidget*> list = currentWidget()->findChildren<QWidget *>();
+
+        foreach(QWidget *w, list)
+        {
+            if (w->isEnabled())
+            {
+                w->setEnabled(false);
+                // mark it as previously enabled
+                d->enabledWidgets[w] = true;
+            }
+        }
+    }
 
     d->widgetList.push_back(child);
     child->winId();
@@ -1346,9 +1362,25 @@ void MythMainWindow::detach(QWidget *child)
     d->widgetList.erase(it);
     QWidget *current = currentWidget();
     if (!current)
+    {
         current = this;
+        // We're be to the main window, enable it just in case
+        setEnabled(true);
+    }
+    else
+    {
+        QList<QWidget*> list = current->findChildren<QWidget *>();
 
-    current->setEnabled(true);
+        foreach(QWidget *w, list)
+        {
+            if (d->enabledWidgets.contains(w))
+            {
+                w->setEnabled(true);
+                d->enabledWidgets.remove(w);
+            }
+        }
+    }
+    current->raise();
     current->setFocus();
     current->setMouseTracking(true);
 

commit 3a4d946901cc36af5f3354fa9fb3e6c1dd044f48

Fix Qt virtual keyboard visibility
Commit 39686c6 caused the Qt keyboard to stop being visible on non-MythUI setting screens. Create ifdefs around the additions for OSX and move setEnabled() back out of the if statement where it was before.

Fixes #12347

mythtv/libs/libmythui/mythmainwindow.cpp
@@ -1391,7 +1391,7 @@ void MythMainWindow::attach(QWidget *child)
             .arg(::GetCurrentThreadId()));
 #endif
     if (currentWidget())
-    {
+#ifdef Q_OS_MAC
         // don't disable the current widget, instead we disable all its children
         // on mac, disabling the current active widget entirely prevent keyboard to
         // work on the newly opened widget.
@@ -1406,8 +1406,9 @@ void MythMainWindow::attach(QWidget *child)
                 d->enabledWidgets[w] = true;
             }
         }
-    }
-
+#else
+        currentWidget()->setEnabled(false);
+#endif
     d->widgetList.push_back(child);
     child->winId();
     child->raise();
@@ -1430,11 +1431,8 @@ void MythMainWindow::detach(QWidget *child)
     d->widgetList.erase(it);
     QWidget *current = currentWidget();
     if (!current)
-    {
         current = this;
-        // We're be to the main window, enable it just in case
-        setEnabled(true);
-    }
+#ifdef Q_OS_MAC
     else
     {
         QList<QWidget*> list = current->findChildren<QWidget *>();
@@ -1449,6 +1447,8 @@ void MythMainWindow::detach(QWidget *child)
         }
     }
     current->raise();
+#endif
+    current->setEnabled(true);
     current->setFocus();
     current->setMouseTracking(true);
 